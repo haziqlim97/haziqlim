d763dfc29864a4387ff699c839a968305d22a1d5
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2019-11-17 04:16:32 +0800

    fix UI

--- resources/views/manage/roles/show.blade.php
+++ resources/views/manage/roles/show.blade.php
@@ -11,36 +11,38 @@
             </div>
             <div class="row justify-content-center">
                 @foreach ($role->permissions->groupBy('name') as $module)
-                    <div class="card col-md-3 m-4">
-                        <div class="card-header">
-                            <h3 class="card-title">{{ collect($module)->first()['name'] }}</h3>
-                            <p class="card-category"> Description : {{ collect($module)->first()['name'] }} </p>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead class=" text-primary">
-                                        <th>
-                                            Permission
-                                        </th>
-                                        <th class="text-right">
-                                        </th>
-                                    </thead>
-                                    <tbody>
+                    <div class="col-md-4">
+                        <div class="card col-md-12">
+                            <div class="card-header">
+                                <h3 class="card-title">{{ collect($module)->first()['name'] }}</h3>
+                                <p class="card-category"> Description : {{ collect($module)->first()['name'] }} </p>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead class=" text-primary">
+                                            <th>
+                                                Permission
+                                            </th>
+                                            <th class="text-right">
+                                            </th>
+                                        </thead>
+                                        <tbody>
 
-                                        @foreach ($module as $permission)
-                                            <tr>
-                                                <td>
-                                                    {{ $permission['permission'] }}
-                                                </td>
-                                                <td class="text-right">
-                                                    <i class="nc-icon nc-key-25"></i>
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                            @foreach ($module as $permission)
+                                                <tr>
+                                                    <td>
+                                                        {{ $permission['permission'] }}
+                                                    </td>
+                                                    <td class="text-right">
+                                                        <i class="nc-icon nc-key-25"></i>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
 
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
