3baa2f68e56516432d3e9a2aba3a7443c2175088
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2019-11-16 22:25:03 +0800

    fix sidebar header

--- resources/views/manage/sidebar.blade.php
+++ resources/views/manage/sidebar.blade.php
@@ -3,12 +3,12 @@
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
     -->
     <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+        <a href="" class="simple-text logo-mini">
             <div class="logo-image-small">
                 <img src="../assets/img/logo-small.png">
             </div>
         </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+        <a href="{{ route('home' )}}" class="simple-text logo-normal">
             {{ config('app.name', 'Laravel') }}
             <!-- <div class="logo-image-big">
               <img src="../assets/img/logo-big.png">
