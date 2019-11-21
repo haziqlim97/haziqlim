a3c1800d658cf6130e98605d830b819a8adbf1fc
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2019-11-22 01:58:50 +0800

    try add stripe uri with dashboard

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,7 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
-        'stripe/*',
+        'https://stripe.com/*',
+        'https://dashboard.stripe.com/',
     ];
 }
