3a153c4bb3fcb4ca596e47b69de3ad60a3d35edb
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2019-11-21 23:44:05 +0800

    hotfix app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,21 +79,3 @@
     </div>
 </body>
 </html>
-
-<script src="https://js.stripe.com/v3/"></script>
-
-<script>
-// window.onload=function(){
-//   document.getElementById("pay-button").click();
-// };
-
-var stripe = Stripe('pk_test_65wAWRCaupNlIbKuGoOxE1hQ00WP11fkr1');
-
-const payButton = document.getElementById('pay-button');
-const sessionId = '{!! $session->id !!}';
-payButton.addEventListener('click', async (e) => {
-    stripe.redirectToCheckout({
-        sessionId: sessionId
-    });
-});
-</script>
