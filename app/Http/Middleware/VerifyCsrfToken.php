eeec5a09a69182894f51ccd98c9e2b40a4b24d06
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2019-11-22 02:02:37 +0800

    another try

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,7 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'https://stripe.com/*',
+        'payment/success',
         'https://dashboard.stripe.com/',
     ];
 }
